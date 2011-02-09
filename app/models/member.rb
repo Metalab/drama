74d6846b000630afc5ccd42ee81354df1dd7678d
System Administrator <fin+github@xbhd.org>
2011-02-09 19:37:06 +0100

    zomg a member!

---
+++ app/models/member.rb
@@ -0,0 +1,13 @@
+class Member
+
+  include DataMapper::Resource
+  # Include default devise modules. Others available are:
+  # :token_authenticatable, :confirmable, :lockable and :timeoutable
+  devise :database_authenticatable, :registerable,
+         :recoverable, :rememberable, :trackable, :validatable
+
+
+  property :id, Serial
+
+
+end
